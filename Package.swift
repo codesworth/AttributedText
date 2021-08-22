96252263647d672a8beace09f27f59ecd5462e6b
codesworth <codezworth@gmail.com>
2021-08-22 23:46:25 +0000

    Fixed Library Folder Structure

--- Package.swift
+++ Package.swift
@@ -21,9 +21,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AttributedText",
-            dependencies: []),
-        .testTarget(
-            name: "AttributedTextTests",
-            dependencies: ["AttributedText"]),
+            dependencies: [], path: "AttributedText/Sources/AttributedText")
     ]
 )
